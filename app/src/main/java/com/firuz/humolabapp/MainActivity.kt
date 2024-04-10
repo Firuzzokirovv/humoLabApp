2e4fd8e16032d29bad7956986e7e3f6d2a673607
firuzzokirov <firuzzokirov33@gmail.com>
2024-04-10 17:52:09 +0500

    add screen_act activity

--- app/src/main/java/com/firuz/humolabapp/MainActivity.kt
+++ app/src/main/java/com/firuz/humolabapp/MainActivity.kt
@@ -1,15 +1,50 @@
 package com.firuz.humolabapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.firuz.second.second_activity
 
 class MainActivity : AppCompatActivity() {
+    private var buttonNewActivity: Button? = null
+    private var editTextView1: EditText? = null
+    private var editTextView2: EditText? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.screen_act)
+        setContentView(R.layout.act3)
+
+        buttonNewActivity = findViewById<Button>(R.id.btn_submit)
+        editTextView1 = findViewById<EditText>(R.id.edit_text_1)
+        editTextView2 = findViewById<EditText>(R.id.edit_text_2)
+
+
+
+    buttonNewActivity?.setOnClickListener {
+        val intent = Intent(this, second_activity::class.java)
+
+
+        if(editTextView1?.text?.isNotEmpty() == true && editTextView2?.text?.isNotEmpty() == true) {
+
+            if(editTextView1?.text?.toString() == editTextView2?.text?.toString()) {
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Пароли не совподают", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(this,"Заполняйте поля", Toast.LENGTH_LONG).show()
+        }
+
+
+        }
+
+
 
     }
-}
+
+
+
+    }
+
---
+++ app/src/main/java/com/firuz/second/second_activity.kt
@@ -0,0 +1,20 @@
+package com.firuz.second
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.firuz.humolabapp.R
+
+class second_activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+
+
+    }
+}
