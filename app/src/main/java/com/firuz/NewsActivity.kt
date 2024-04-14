3f4b9745cd13744242f3438b70051c7487098057
firuzzokirov <firuzzokirov33@gmail.com>
2024-04-14 23:02:42 +0500

    create ProfileActivity and profile_activity.xml. and Make Clickable TextView

--- app/src/main/java/com/firuz/NewsActivity.kt
+++ app/src/main/java/com/firuz/NewsActivity.kt
@@ -1,13 +1,39 @@
 package com.firuz
 
+
+import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import com.firuz.humolabapp.R
+import com.firuz.second.second_activity
 
 class NewsActivity : AppCompatActivity() {
+
+    private var cardView1: CardView? = null
+    private var cardView2: CardView? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        cardView1 = findViewById<CardView>(R.id.card_1)
+        cardView2 = findViewById<CardView>(R.id.card_2)
+
+
+        cardView1?.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        cardView2?.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
