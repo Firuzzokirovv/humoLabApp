091f59fcdc1ccf1505dc018ce2181f82d12bb9c1
firuzzokirov <firuzzokirov33@gmail.com>
2024-04-17 19:54:18 +0500

    изменил фото профиля

--- app/src/main/java/com/firuz/NewsActivity.kt
+++ app/src/main/java/com/firuz/NewsActivity.kt
@@ -3,8 +3,12 @@ package com.firuz
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.firuz.humolabapp.R
 import com.firuz.second.second_activity
 
@@ -12,6 +16,7 @@ class NewsActivity : AppCompatActivity() {
 
     private var cardView1: CardView? = null
     private var cardView2: CardView? = null
+    private var numberPanel: RecyclerView? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +24,11 @@ class NewsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
 
+
         cardView1 = findViewById<CardView>(R.id.card_1)
         cardView2 = findViewById<CardView>(R.id.card_2)
+        numberPanel = findViewById(R.id.numberPanel)
+        numberPanel?.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
 
 
         cardView1?.setOnClickListener {
